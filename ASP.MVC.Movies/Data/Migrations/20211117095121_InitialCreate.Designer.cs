fcefa490041042e9dcddefd0796c5a0304af9168
hakee <hakee@LAPTOP-84U9VAUD>
2021-11-17 09:58:39 +0000

    Migration update

--- ASP.MVC.Movies/Data/Migrations/20211117095121_InitialCreate.Designer.cs
+++ ASP.MVC.Movies/Data/Migrations/20211117095121_InitialCreate.Designer.cs
@@ -4,14 +4,16 @@ using ASP.MVC.Movies.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ASP.MVC.Movies.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211117095121_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
